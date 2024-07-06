4f1ec49a8235f51cd0ddaac9cc3df76dfaceb214
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-06 18:18:32 +0300

    To-do List completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    creatVal(e.target.new.value)
+  })
 });
+
+function creatVal(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', btnDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+
+console.log(li)
+document.querySelector('#tasks').appendChild(li)
+}
+
+function btnDelete(e){
+  e.target.parentNode.remove()
+}
